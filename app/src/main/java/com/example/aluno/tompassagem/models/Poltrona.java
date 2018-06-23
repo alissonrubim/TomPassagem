e118c140d44188a1cff1ce01df58745cff509e10
Pedro <pedropioxii@gmail.com>
2018-06-22 21:14:10 -0300

    add model poltrona

---
+++ app/src/main/java/com/example/aluno/tompassagem/models/Poltrona.java
@@ -0,0 +1,54 @@
+package com.example.aluno.tompassagem.models;
+
+/**
+ * Created by aluno on 22/06/2018.
+ */
+
+public class Poltrona {
+
+    String id, assento;
+    Boolean ocupado;
+    Usuario usur;
+
+    public Poltrona() {
+    }
+
+    public Poltrona(String id, String assento, Boolean ocupado, Usuario usur) {
+        this.id = id;
+        this.assento = assento;
+        this.ocupado = ocupado;
+        this.usur = usur;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAssento() {
+        return assento;
+    }
+
+    public void setAssento(String assento) {
+        this.assento = assento;
+    }
+
+    public Boolean getOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(Boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public Usuario getUsur() {
+        return usur;
+    }
+
+    public void setUsur(Usuario usur) {
+        this.usur = usur;
+    }
+}
